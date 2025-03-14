f49fa48ee020704bdd8b6126e4129448f2ef19a6
Jack Genchi <jackgenchi@gmail.com>
2025-03-13 19:59:08 -0700

    3.13solutions

---
+++ js/script.js
@@ -0,0 +1,15 @@
+window.addEventListener('load', init) // handle load event
+function init () {
+    const $ = (id) => document.getElementById(id); //helper function
+    let test = $('empForm'); // load full form object to test
+    test.addEventListener('submit', (e) => { //event listener on event "submit"
+        for (let i of e.target) {
+            if(i.id === "submit")
+                break;
+            console.log(i.id + ": " + i.value);
+        } //iterate through all form array elements
+        e.preventDefault(); //prevent default behavior
+    });
+}
+
+
